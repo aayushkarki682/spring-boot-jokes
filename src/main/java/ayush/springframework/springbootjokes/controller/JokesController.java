4ab49c9fd4af3ed3b318780d13d1305a0bc0b776
aayushkarki682 <ayush.karki12345@gmail.com>
2020-12-31 01:59:14 -0600

    mapping context root ("/") to jokes view. closes #4

--- src/main/java/ayush/springframework/springbootjokes/controller/JokesController.java
+++ src/main/java/ayush/springframework/springbootjokes/controller/JokesController.java
@@ -1,8 +1,13 @@
 package ayush.springframework.springbootjokes.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokesController {
 
+    @RequestMapping("/")
+    public String viewJokes(){
+        return "";
+    }
 }
