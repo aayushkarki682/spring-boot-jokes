f86b48bb279de487e1cd1798040136e9643fd6f1
aayushkarki682 <ayush.karki12345@gmail.com>
2020-12-31 02:12:30 -0600

    completing controller class to return chucknorris view. closes #6

--- src/main/java/ayush/springframework/springbootjokes/controller/JokesController.java
+++ src/main/java/ayush/springframework/springbootjokes/controller/JokesController.java
@@ -1,13 +1,21 @@
 package ayush.springframework.springbootjokes.controller;
 
+import ayush.springframework.springbootjokes.services.RandomQuoteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokesController {
 
+    @Autowired
+    private RandomQuoteService randomQuoteService;
+
     @RequestMapping("/")
-    public String viewJokes(){
-        return "";
+    public String viewJokes(Model model){
+
+        model.addAttribute("joke", randomQuoteService.getJokeQuote());
+        return "chucknorris";
     }
 }
--- src/main/java/ayush/springframework/springbootjokes/services/RandomQuoteServiceImpl.java
+++ src/main/java/ayush/springframework/springbootjokes/services/RandomQuoteServiceImpl.java
@@ -1,6 +1,9 @@
 package ayush.springframework.springbootjokes.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RandomQuoteServiceImpl implements RandomQuoteService {
     @Override
     public String getJokeQuote() {
